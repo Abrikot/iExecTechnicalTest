2c52709fe9db10cadc077b4eee3ea04d4b06c866
Maxence Cornaton <cornatonmaxence@gmail.com>
2021-04-30 18:07:41 +0200

    Initial commit

---
+++ src/main/java/cornaton/maxence/technicaltest/iexec/IexecApplication.java
@@ -0,0 +1,13 @@
+package cornaton.maxence.technicaltest.iexec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IexecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cornaton/maxence/technicaltest/iexec/IexecApplicationTests.java
@@ -0,0 +1,13 @@
+package cornaton.maxence.technicaltest.iexec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IexecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
